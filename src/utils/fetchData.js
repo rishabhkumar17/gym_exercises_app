546083372a594c657552587695c7093b366fcf8a
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-09-29 23:16:09 +0530

    fetch data function

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,1 +1,14 @@
-export const fetchData = async () => {}
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+  },
+}
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options)
+  const data = await response.json()
+
+  return data
+}
