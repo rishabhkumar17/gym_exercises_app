04c4cb1a70223434e768af959f997de504723049
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-09-29 05:55:39 +0530

    styling using meterial UI

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
 const SearchExercises = () => {
-  return <div>SearchExercises</div>
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises You <br />
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '1170px', xs: '350px' },
+            backgroundColor: '#ffff',
+            borderRadius: '40px'
+          }}
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder="Search Exercises"
+          type="text"
+        />
+      </Box>
+    </Stack>
+  )
 }
 
 export default SearchExercises
