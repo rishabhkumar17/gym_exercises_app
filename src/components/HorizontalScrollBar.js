8770846b6c526a6a1045d0496a026c024ffb113d
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-09-30 21:07:23 +0530

    Horizontal Scroll Bar updated

--- src/components/HorizontalScrollBar.js
+++ src/components/HorizontalScrollBar.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Box } from '@mui/material'
 
-const HorizontalScrollBar = () => {
-  return <div>HorizontalScrollBar</div>
+const HorizontalScrollBar = ({ data }) => {
+  console.log(data)
+  return (
+    <div>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m="0 40px"
+        >
+          {item}
+        </Box>
+      ))}
+    </div>
+  )
 }
 
 export default HorizontalScrollBar
