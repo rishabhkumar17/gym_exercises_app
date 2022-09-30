d590e2c3fb57a7cadf128f84bfab220cfc7e370f
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-09-30 23:27:29 +0530

    bodyPart-card updated

---
+++ src/components/BodyPart.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Stack, Typography } from '@mui/material'
+
+import Icon from '../assets/icons/gym.png'
+
+const BodyPart = ({ item, bodyPart, setbodyPart }) => {
+  return (
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+        backgroundColor: '#fff',
+        borderBottomLeftRadius: '20px',
+        width: '270px',
+        height: '280px',
+        cursor: 'pointer',
+        gap: '47px',
+      }}
+    >
+      <img
+        src={Icon}
+        alt="dumbbell"
+        styles={{ width: '40px', height: '40px' }}
+      />
+    </Stack>
+  )
+}
+
+export default BodyPart
