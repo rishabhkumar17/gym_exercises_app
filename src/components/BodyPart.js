bdb03c835fd240714c5d246746fdd44c1669d745
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-01 00:03:18 +0530

    bodyPart-card updated

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,7 +3,7 @@ import { Stack, Typography } from '@mui/material'
 
 import Icon from '../assets/icons/gym.png'
 
-const BodyPart = ({ item, bodyPart, setbodyPart }) => {
+const BodyPart = ({ item, bodyPart, setBodyPart }) => {
   return (
     <Stack
       type="button"
@@ -19,12 +19,24 @@ const BodyPart = ({ item, bodyPart, setbodyPart }) => {
         cursor: 'pointer',
         gap: '47px',
       }}
+      onClick={() => {
+        setBodyPart(item)
+        window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' })
+      }}
     >
       <img
         src={Icon}
         alt="dumbbell"
-        styles={{ width: '40px', height: '40px' }}
+        style={{ width: '40px', height: '40px' }}
       />
+      <Typography
+        fontSize="24px"
+        fontweight="bold"
+        color="#3A1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
     </Stack>
   )
 }
