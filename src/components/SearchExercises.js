7e79863a57dba76eb736f15e57ae223a19622145
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-09-30 23:22:59 +0530

    props added

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,9 +4,9 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 import { exerciseOptions, fetchData } from '../utils/fetchData'
 import HorizontalScrollBar from './HorizontalScrollBar'
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState('')
-  const [exercises, setExercises] = useState([])
+
   const [bodyParts, setBodyParts] = useState([])
 
   useEffect(() => {
@@ -83,7 +83,11 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-        <HorizontalScrollBar data={bodyParts} />
+        <HorizontalScrollBar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   )
