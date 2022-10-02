fd3dc6eb478e200739c16c5366652e2d8ff0a467
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-03 01:36:27 +0530

    fetch Exercise Data

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -4,11 +4,31 @@ import { Box } from '@mui/material'
 
 import { exerciseOptions, fetchData } from '../utils/fetchData'
 import { Details } from '@mui/icons-material'
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({})
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
+      const youtubeSearchUrl =
+        'https://youtube-search-and-download.p.rapidapi.com'
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/${id}`,
+        exerciseOptions
+      )
+      setExerciseDetail(exerciseDetailData)
+    }
+    fetchExerciseData()
+  }, [id])
+
   return (
     <Box>
-      <Details />
+      <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideos />
       <SimilarExercises />
     </Box>
