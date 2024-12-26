d27d043de668d852fd03503431a4b8eae8ffcf92
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-12-26 14:05:13 +0530

    API KEY update build

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,16 +1,16 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+    'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
   },
 }
 
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+    'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com',
   },
 }
 
